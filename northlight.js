a9429ffe38cc1e72bef3ce396a4fae117eda120c
dyutiiigupta <aditi.gupta70303@gmail.com>
2026-07-14 23:47:59 +0530

    Add files via upload

--- northlight.js
+++ northlight.js
@@ -2,3 +2,9 @@ document.querySelector(".menu").addEventListener("click",()=>{
     document.querySelector(".container").classList.toggle("change");
 
 })
+const links=document.querySelectorAll(".menu-link");
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        document.querySelector(".container").classList.remove("change");
+    });
+});
